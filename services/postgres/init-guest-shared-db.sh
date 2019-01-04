16868d1078422beb56b318d4e8d1de51582f2b71
Alexander Junge <alexander.junge@gmail.com>
2019-01-04 16:30:56 +0100

    Towards adding something to psql database

--- services/postgres/init-guest-shared-db.sh
+++ services/postgres/init-guest-shared-db.sh
@@ -4,5 +4,8 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER guest;
     CREATE DATABASE shared;
-    GRANT SELECT ON DATABASE shared TO guest;
+    CREATE TABLE data (id integer, label text, score float);
+    \copy data FROM 'data.tsv';
+    CREATE INDEX data_idx ON data (id);
+    GRANT SELECT ON ALL TABLES IN SCHEMA public TO guest;
 EOSQL
