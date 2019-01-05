ab8328679f3c5280a97400ed8e9a22e57062cd0f
Alexander Junge <alexander.junge@gmail.com>
2019-01-05 15:53:38 +0100

    Fix path to data file used to initialize db

--- services/postgres/init-guest-shared-db.sh
+++ services/postgres/init-guest-shared-db.sh
@@ -5,7 +5,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER guest;
     CREATE DATABASE shared;
     CREATE TABLE data (id integer, label text, score float);
-    \copy data FROM 'data.tsv';
+    \copy data FROM '/docker-entrypoint-initdb.d/data.tsv';
     CREATE INDEX data_idx ON data (id);
     GRANT SELECT ON ALL TABLES IN SCHEMA public TO guest;
 EOSQL
