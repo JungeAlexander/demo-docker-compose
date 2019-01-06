08a45e1e992b7b04c0c1eaa97c94dd09913fb42d
Alexander Junge <alexander.junge@gmail.com>
2019-01-06 10:48:28 +0100

    Fix script by connecting to right DB

--- services/postgres/init-guest-shared-db.sh
+++ services/postgres/init-guest-shared-db.sh
@@ -2,10 +2,12 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER guest;
     CREATE DATABASE shared;
+    \connect shared;
+    CREATE USER guest;
     CREATE TABLE data (id integer, label text, score float);
     \copy data FROM '/docker-entrypoint-initdb.d/data.tsv';
     CREATE INDEX data_idx ON data (id);
     GRANT SELECT ON ALL TABLES IN SCHEMA public TO guest;
+    ANALYZE VERBOSE;
 EOSQL
