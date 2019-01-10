ca16b1e47a0ea6b8afca00ebba744eefd75a11ba
Alexander Junge <alexander.junge@gmail.com>
2019-01-10 15:13:27 +0100

    Retry talking to DB in dashboard app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,12 +2,31 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import logging
 import pandas as pd
 import requests
+import time
 from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+
+logging.basicConfig(level=logging.INFO)
 
 engine = create_engine('postgresql://guest@localhost:8001/shared')
-df = pd.read_sql_query('select * from data', con=engine)
+num_tries = 3
+current_tries = 0
+df = None
+while True:
+    logging.info(f"Trying to connect to psql DB. Try {current_tries}.")
+    try:
+        df = pd.read_sql_query('select * from data', con=engine)
+        break
+    except OperationalError as err:
+        current_tries += 1
+        if current_tries <= num_tries:
+            time.sleep(10)
+        else:
+            raise err
+
 predictions = []
 for val in df['score']:
     r = requests.get(f'http://127.0.0.1:4000/predict/{val}')
